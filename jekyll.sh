5a84779a4d48b1c0ce8ca4a734a656ddd218a631
terence edwards <theterenceedwards@gmail.com>
2018-01-15 10:33:23 -0800

    project cleanup

--- jekyll.sh
+++ jekyll.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "View your site at: http://127.0.0.1:4000/"
-docker run -v $(pwd):/srv/jekyll -p 127.0.0.1:4000:4000 jekyll/jekyll:pages
+docker run --rm -v $(pwd):/srv/jekyll -p 4000:4000 -it jekyll/jekyll jekyll s
